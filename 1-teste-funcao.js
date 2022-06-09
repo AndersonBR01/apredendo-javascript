35af59011c0f8650414317b334cb7ff03ad0b866
Anderson <andersoncsilva850@gmail.com>
2022-06-09 11:47:29 -0300

    teste

--- 1-teste-funcao.js
+++ 1-teste-funcao.js
@@ -1,5 +1,22 @@
-function soma(a, b) {
+/*function soma(a, b) {
+    console.log("hello word!!")
     return a + b; //8
 }
 
-soma (3, 5) ;
+soma (3, 5) ;*/
+
+function returnEvenValues(array) {
+    let evenNums = [];
+    for (let i = 0; i < array.length; i++ ){
+        if(array[i] % 2 === 0) {
+            evenNums.push(array[i]);
+        }
+    }
+    console.log('os numeros pares são:' , evenNums);
+}
+
+let array = [1,2,3,4,5,7,8];
+
+returnEvenValues(array);
+
+
