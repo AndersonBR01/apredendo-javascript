059261c89f0a53ab784e564d1ebf7c1d3606245a
Anderson <andersoncsilva850@gmail.com>
2022-06-12 15:40:41 -0300

    Update arrayPares.js

--- praticando/arrayPares.js
+++ praticando/arrayPares.js
@@ -1,9 +1,8 @@
 function substituiPares(array){
-    if(!array) return -1;
+    if(!array) return-1;
 
     if(!array.length) return-1;
-
-
+    
     for(let i = 0; i < array.length; i++){
         if(array[i] === 0){
             console.log("voçé já é zero!!")
@@ -17,4 +16,4 @@ function substituiPares(array){
 
 let arr =[1, 3, 4, 6, 80, 33, 23, 90];
 
-console.log(substituiPares(null));
+console.log(substituiPares(arr));
