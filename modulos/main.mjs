00b76f31fcbf424e44c23a7750fe77222a602222
Anderson <andersoncsilva850@gmail.com>
2022-06-19 19:42:06 -0300

    modulos .js e .mjs

---
+++ modulos/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraCidade("Anderson", "Cajamar"));
+console.log(mostraHobby("Anderson", "Progamação"));
+console.log(mostraIdade("And", 26))
